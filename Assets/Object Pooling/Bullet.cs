148310f48dad55fc912384ddd7dc8586780fca1a
rian-test-sourcetree <fitransyah.rusman@gmail.com>
2022-02-09 17:08:59 +0700

    setting object pooling for launcher and bullet script

--- Assets/Object Pooling/Bullet.cs
+++ Assets/Object Pooling/Bullet.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Bullet : MonoBehaviour {
     [SerializeField] Vector3 speed;
+    private IObjectPool<Bullet> bulletPool;
+
+    public void SetPool(IObjectPool<Bullet> pool)
+    {
+        bulletPool = pool;
+    }
 
     private void Update() {
         transform.position += speed * Time.deltaTime;
     }
 
     private void OnBecameInvisible() {
-        Destroy(gameObject);
+        bulletPool.Release(this);
     }
 }

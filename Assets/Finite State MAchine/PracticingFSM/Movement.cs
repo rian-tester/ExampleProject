cdd0f17936d834c070fe2c78ecad56df1e7194ea
rian-test-sourcetree <fitransyah.rusman@gmail.com>
2022-02-16 03:02:54 +0700

    fixing the movement script

--- Assets/Finite State MAchine/PracticingFSM/Movement.cs
+++ Assets/Finite State MAchine/PracticingFSM/Movement.cs
@@ -6,33 +6,19 @@ public class Movement : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float rotationSpeed = 50f;
 
-    private Rigidbody2D myRigidBody;
-
-    public event Action onMoving;
-    public event Action onRotating;
-    public event Action onStopping;
-    private void Start()
-    {
-        myRigidBody = GetComponent<Rigidbody2D>();
-    }
     void Update()
     {
-        if (myRigidBody.velocity == Vector2.zero) onStopping();
         Moving();
         Rotating();
     }
     void Moving()
     {
         float translation = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.Translate(0,translation,0);
-
-        if (translation > 0 || translation < 0) onMoving();       
+        transform.Translate(0,translation,0);      
     }
     void Rotating()
     {
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime *-1f;
         transform.Rotate(0, 0, rotation);
-
-        if (rotation > 0 || rotation < 0) onRotating(); 
     }
 }

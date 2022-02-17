d97b2b9dbb235cf78bd000fdc7b318d633a9810f
rian-test-sourcetree <fitransyah.rusman@gmail.com>
2022-02-17 23:27:03 +0700

    connecting game mechanic and the state machine

--- Assets/Finite State MAchine/PracticingFSM/Movement.cs
+++ Assets/Finite State MAchine/PracticingFSM/Movement.cs
@@ -6,19 +6,38 @@ public class Movement : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float rotationSpeed = 50f;
 
+    private Rigidbody2D myRigidbody;
+
+    public event Action onMoving;
+    public event Action onRotating;
+    public event Action onStill;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
+        if (myRigidbody.velocity == Vector2.zero)
+        {
+            if (onStill != null) onStill();
+        }
         Moving();
         Rotating();
+
     }
     void Moving()
     {
         float translation = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.Translate(0,translation,0);      
+        if (translation == 0) return;
+        transform.Translate(0,translation,0); 
+        if (onMoving != null) onMoving();
     }
     void Rotating()
     {
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime *-1f;
+        if (rotation == 0) return;
         transform.Rotate(0, 0, rotation);
+        if (onRotating != null) onRotating();
     }
 }
--- Assets/Finite State MAchine/PracticingFSM/StateMachinePattern.cs
+++ Assets/Finite State MAchine/PracticingFSM/StateMachinePattern.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class StateMachinePattern : MonoBehaviour, StateContext
 {
-    private BaseState currentState;
+    private BaseState currentState = new IdleState();
+    [SerializeField] private Movement movement;
+
     public void SetState(BaseState newState)
     {
         currentState = newState;
@@ -11,4 +13,16 @@ public class StateMachinePattern : MonoBehaviour, StateContext
     void Moving() => currentState.Moving(this);
     void Stopping() => currentState.Stopping(this);
     void Turning() => currentState.Turning(this);
+    private void OnEnable()
+    {
+        movement.onMoving += Moving;
+        movement.onStill += Stopping;
+        movement.onRotating += Turning;
+    }
+    private void OnDisable()
+    {
+        movement.onMoving -= Moving;
+        movement.onStill -= Stopping;
+        movement.onRotating -= Turning;
+    }
 }

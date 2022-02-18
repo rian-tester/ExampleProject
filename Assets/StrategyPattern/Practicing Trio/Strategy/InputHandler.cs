2f6c9c8caaa7e44339eee3871f0013dedde238c3
rian-test-sourcetree <fitransyah.rusman@gmail.com>
2022-02-18 20:55:53 +0700

    making it into one sequence + observer pattern

--- Assets/StrategyPattern/Practicing Trio/Strategy/InputHandler.cs
+++ Assets/StrategyPattern/Practicing Trio/Strategy/InputHandler.cs
@@ -3,26 +3,16 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
-    [SerializeField] SquareAbilitySystem abilitySystem;
+    public event Action onBlinking;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            abilitySystem.SetTheAbiltyType(new ParticleAbility());
+            if (onBlinking != null) onBlinking();
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            abilitySystem.SetTheAbiltyType(new ColorAbility());
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            abilitySystem.SetTheAbiltyType
-                (
-                    new DelayerDecorator 
-                    (
-                        new ParticleAbility()
-                    )
-                );
+
         }
     }
 }
--- Assets/StrategyPattern/Practicing Trio/Strategy/SquareAbilitySystem.cs
+++ Assets/StrategyPattern/Practicing Trio/Strategy/SquareAbilitySystem.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class SquareAbilitySystem : MonoBehaviour
 {
+    [SerializeField] InputHandler input;
+
     private ISquareAbility currentAbility = null;
 
     public ParticleSystem ParticleEffect;
     public SpriteRenderer SpriteRenderer;
+    public Vector2 forward = new Vector2(3f, 0);
+    public Vector2 vector2 = new Vector2(-3f, 0);
 
-    public void SetTheAbiltyType (ISquareAbility abilityType)
+    private void SetTheAbiltyType (ISquareAbility abilityType)
     {
         currentAbility = abilityType;
         currentAbility?.UseAbility(this);
+    }
+    private void Blinking()
+    {
+        SetTheAbiltyType
+            (new AbilitySequence(new ISquareAbility[]
+            {
+                new ParticleAbility(),
+                new ColorAbility(),
+                new DelayerDecorator (new ColorAbility())
+            }));
+    }
+    private void OnEnable()
+    {
+        input.onBlinking += Blinking;
+    }
+    private void OnDisable()
+    {
+        
     }
 }

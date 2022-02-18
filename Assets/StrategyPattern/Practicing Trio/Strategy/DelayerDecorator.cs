127598a599a3a9bbcaba45caa73569f974d007e5
rian-test-sourcetree <fitransyah.rusman@gmail.com>
2022-02-18 18:40:02 +0700

    setup exercise for decorator with using Strategy pattern (variation version)

---
+++ Assets/StrategyPattern/Practicing Trio/Strategy/DelayerDecorator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayerDecorator : ISquareAbility
+{
+    public void UseAbility(SquareAbilitySystem abilitySystem)
+    {
+        throw new System.NotImplementedException();
+    }
+}
